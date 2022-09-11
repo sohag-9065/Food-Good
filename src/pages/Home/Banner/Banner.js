1fbb9c201aa98df477f2feac0b63844bfe68e534
Sohag <sohaghossain7th@gmail.com>
2022-09-11 09:54:03 +0600

    banner opasity change

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -6,20 +6,20 @@ const Banner = () => {
     const backgroundImageStyle = {
         height: '400px',
         backgroundImage: `url(${BannerBackground})`,
-        opacity: 1,
+        filter: 'opacity(1)',
         backgroundSize: 'cover',
     };
     return (
         <div>
             {/* <Hero style={{ backgroundImage: `url(${BannerBackground})`, }} ></Hero> */}
-            <Hero
+            {/* <Hero
                 style={backgroundImageStyle}
             >
                 <Hero.Overlay />
 
                 <Hero.Content className="text-center w-full">
                     <div className="flex justify-center w-full ">
-                        
+
                         <Form className='w-8/12 items-center'>
                             <h2 className='text-5xl mb-8 font-semibold'>Best food waiting for your belly</h2>
                             <InputGroup className='max-w-2xl justify-center' >
@@ -29,7 +29,16 @@ const Banner = () => {
                         </Form>
                     </div>
                 </Hero.Content>
-            </Hero>
+            </Hero> */}
+            <div style={backgroundImageStyle} className='flex items-center justify-center'>
+                <Form className='w-full items-center justify-center'>
+                    <h2 className='text-5xl mb-8 font-semibold'>Best food waiting for your belly</h2>
+                    <InputGroup className='max-w-2xl justify-center' >
+                        <Input type="text" placeholder="Search food item" className='w-6/12' bordered />
+                        <span>Search</span>
+                    </InputGroup>
+                </Form>
+            </div>
 
         </div>
     );
